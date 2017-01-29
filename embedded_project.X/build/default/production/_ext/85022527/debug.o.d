f3e2f20e8e0619ef011115e625842401fc40d4ae
ur mom <mpm8128@vt.edu>
2017-01-29 15:05:05 -0500

    Put things back in Harmony so the configurator works, etc.

---
+++ embedded_project.X/build/default/production/_ext/85022527/debug.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/85022527/debug.o:  \
+ ../../../../../../../Users/user/Documents/Repositories/Embedded/src/debug.c  \
+ ../../../../../../../Users/user/Documents/Repositories/Embedded/src/debug.h 
